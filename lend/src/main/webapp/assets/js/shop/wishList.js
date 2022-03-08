109b97599f00e8642dd2a237455943583d39eb4d
karta105302 <karta105302@gmail.com>
2022-03-08 21:41:03 +0800

    add wishlist

---
+++ lend/src/main/webapp/assets/js/shop/wishList.js
@@ -0,0 +1,32 @@
+document.addEventListener("DOMContentLoaded", function (event) {
+    let allwish = localStorage.getItem("wish") ? JSON.parse(localStorage.getItem("wish")) : [];
+
+    for (let i = 0; i < allwish.length; i++) {
+
+        let showWishList =
+        `<tr>
+            <td class="thumbnail"><a href="product-details.jsp"><img src="${allwish[i].productImage}" alt="wishlist-product-1"></a></td>
+            <td class="name"> <a href="product-details.jsp">${allwish[i].productName}</a></td>
+            <td class="price"><span>NT ${allwish[i].productPrice}</span></td>
+            <td class="add-to-cart"><a href="#" class="btn btn-light btn-hover-dark"><i class="fal fa-shopping-cart mr-2"></i>放入購物車</a></td>
+            <td class="remove wishRemove"><a href="#" class="btn">×</a></td>
+        </tr>`
+    $(".allWishList").append(showWishList);
+    }
+
+    //移除
+	$(document).off("click").on("click",".wishRemove", function () {
+		$(this).closest("tr").fadeOut(0, function () {
+			let check = $(this).closest("tr").find(".name").children("a").text();
+			let cartAll = localStorage.getItem("wish") ? JSON.parse(localStorage.getItem("wish")) : [];
+			for (let i = 0; i < cartAll.length; i++) {
+				if (cartAll[i].productName.match(check) != null) {
+					cartAll.splice(i, 1)
+				}
+			}
+			$(this).closest("tr").remove();
+			localStorage.setItem("wish", JSON.stringify(cartAll));
+			$("span.wishlist-count").html(cartAll.length);
+		})
+    })
+})
