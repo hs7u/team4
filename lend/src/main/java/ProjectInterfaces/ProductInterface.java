63b0a5886a18313a0349da92d286b625ba475659
CL <clare25tw@gmail.com>
2022-03-08 15:34:58 +0800

    fix

--- lend/src/main/java/ProjectInterfaces/ProductInterface.java
+++ lend/src/main/java/ProjectInterfaces/ProductInterface.java
@@ -4,13 +4,21 @@ import java.io.Serializable;
 import java.util.ArrayList;
 
 public interface ProductInterface<ProductVO> {
-    public Serializable insert(ProductVO pVo);
-    public Boolean update(ProductVO pVo);
-    public void delete(Integer productId);
-    public void sold(Integer productId, Integer sold);
-    public void changeStatus(Integer productId, Byte statusCode);
-    public ProductVO selectByProductId(Integer productId);
-    public byte[] selectPhotoByProductId(Integer productId);
-    public ArrayList<ProductVO> getAllProduct();
-    public ArrayList<ProductVO> selectCategoryByProduct();
+	public Serializable insert(ProductVO pVo);
+
+	public Boolean update(ProductVO pVo);
+
+	public void delete(Integer productId);
+
+	public void sold(Integer productId, Integer sold);
+
+	public void changeStatus(Integer productId, Byte statusCode);
+
+	public ProductVO selectByProductId(Integer productId);
+
+	public byte[] selectPhotoByProductId(Integer productId);
+
+	public ArrayList<ProductVO> getAllProduct();
+
+	public ArrayList<ProductVO> selectProductByCategory(Integer productCategoryCode, String orderByCondition);
 }
